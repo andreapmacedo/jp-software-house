6566e93450a65d9c517f91d98bb869677c00a5e7
andreapmacedo <aa.andremacedo@gmail.com>
2023-02-17 14:19:31 -0300

    feat: Implementa requisição para api do 'themoviedb'.

---
+++ src/services/apiFilmes.tsx
@@ -0,0 +1,81 @@
+// import dotenv from 'dotenv';
+// import dotenv from 'react-native-dotenv';
+// require('dotenv').config({ path: `${__dirname}/../../config.env` }) 
+// require('dotenv').config();
+// dotenv.config();
+
+// import { API_KEY } from '@env'
+
+
+import axios from 'axios';
+
+const apiKey = ''
+const apiKeyE = process.env.API_KEY;
+
+const baseURL = 'https://api.themoviedb.org/3';
+
+export interface IToken {
+  request_token: string;
+}
+
+export async function getMovieById(movieId: number) {
+  console.log(apiKeyE);
+  console.log('teste');
+  
+  // const response = await axios.get(`${baseURL}/movie/${movieId}`, {
+  //   params: { api_key: apiKey },
+  // });
+  // return response.data;
+}
+
+export async function getPopularMovies() {
+  const response = await axios.get(`${baseURL}/movie/popular`, {
+    params: { api_key: apiKey },
+  });
+  return response.data;
+}
+
+export function listarFilmesPopulares(page: number) {
+  // return http.get<Results>(this.baseURL + 'popular?' + this.apiKey + '&page=' + page)
+  // return this.http.get<Results>(this.baseURL + 'popular?' + this.apiKey + '&page=' + page)
+}
+
+export function listarMelhoresAvaliados(page: number) {
+  // return this.http.get<Results>(this.baseURL + 'top_rated?' + this.apiKey + '&page=' + page)
+}
+
+export function  listarFilmesNosCinemas() {
+  // return this.http.get<Results>(this.baseURL + 'now_playing?' + this.apiKey + '&page=1&region=BR')
+}
+
+export function filtrarFilmes(nomeFilme: string) {
+  // return this.http.get<Results>(this.filtroURL + nomeFilme)
+}
+
+export function  getFilmeById(idFilme: number) {
+  // return this.http.get<Filme>(this.baseURL + idFilme + '?' + this.apiKey)
+}
+
+export function getCreditsFilme(idFilme: number) {
+  // return this.http.get<Filme>(this.baseURL + idFilme + '/credits?' + this.apiKey)
+}
+
+export function getProviders(idFilme: number) {
+  // return this.http.get<Provider>(this.baseURL + idFilme + '/watch/providers?' + this.apiKey)
+}
+
+export function getGenres() {
+  // return this.http.get<Genre>('https://api.themoviedb.org/3/genre/movie/list?' + this.apiKey)
+}
+
+export function discoverMovies(page: number) {
+  // return this.http.get<Results>('https://api.themoviedb.org/3/discover/movie?' + this.apiKey + '&include_adult=false&sort_by=vote_count.desc&page=' + page)
+}
+
+export function autenticarUsuarioPorToken() {
+  // return this.http.get<Token>('https://api.themoviedb.org/3/authentication/token/new?' + this.apiKey)
+}
+
+export function createSession(token: IToken) {
+  // return this.http.post<Session>('https://api.themoviedb.org/3/authentication/session/new?' + this.apiKey, token)
+}
