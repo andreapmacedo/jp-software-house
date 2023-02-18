8fbf694b4acc43195ce4d6db0aff94e76c496bf3
andreapmacedo <aa.andremacedo@gmail.com>
2023-02-18 16:35:43 -0300

    feat: Finaliza estilo dos 'inputs' da tela de login.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -56,47 +56,49 @@ export default function SignIn() {
         delay={500}
         style={styles.containerForm}
       >
-        <TouchableHighlight
-          onPress={handleFocus}
-          style={[styles.wrapper, focused ? styles.wrapperFocused : null]}
-          underlayColor={'rgba(29, 70, 114, 0.836)'}
-        >
-          <TextInput        
-            onBlur={handleBlur}
-            ref={inputRef}
-            placeholder="Digite seu e-mail"
-            placeholderTextColor="#cacaca"
-            style={styles.input}
-            value={email}
-            onChangeText={setEmail}
-           />
-        </TouchableHighlight>
+        {/* <View style={styles.loginContainer}> */}
+        
+          <TouchableHighlight
+            onPress={handleFocus}
+            style={[styles.wrapper, focused ? styles.wrapperFocused : null]}
+            underlayColor={'rgba(29, 70, 114, 0.836)'}
+          >
+            <TextInput        
+              onBlur={handleBlur}
+              ref={inputRef}
+              placeholder="Digite seu e-mail"
+              placeholderTextColor="#cacaca"
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+            />
+          </TouchableHighlight>
 
-        <TouchableHighlight
-          onPress={handleFocus}
-          style={[styles.wrapper, focused ? styles.wrapperFocused : null]}
-          underlayColor={'rgba(29, 70, 114, 0.836)'}
-        >
-          <TextInput        
-            onBlur={handleBlur}
-            ref={inputRef}
-            placeholder="Sua senha"
-            style={styles.input}
-            placeholderTextColor="#cacaca"
-            value={password}
-            secureTextEntry={true}
-            onChangeText={setPassword}
-           />
-        </TouchableHighlight>
+          <TouchableHighlight
+            onPress={handleFocus}
+            style={[styles.wrapper, focused ? styles.wrapperFocused : null]}
+            underlayColor={'rgba(29, 70, 114, 0.836)'}
+          >
+            <TextInput        
+              onBlur={handleBlur}
+              ref={inputRef}
+              placeholder="Sua senha"
+              style={styles.input}
+              placeholderTextColor="#cacaca"
+              value={password}
+              secureTextEntry={true}
+              onChangeText={setPassword}
+            />
+          </TouchableHighlight>
 
-        <TouchableHighlight
-          style={styles.buttonRegister}
-          onPress={handleLogin}
-          underlayColor={'rgba(29, 70, 114, 0.836)'}  
-        >
-          <Text style={styles.registerText}>Entrar</Text>
-        </TouchableHighlight>
-
+          <TouchableHighlight
+            style={styles.buttonRegister}
+            onPress={handleLogin}
+            underlayColor={'rgba(29, 70, 114, 0.836)'}  
+          >
+            <Text style={styles.buttonRegisterText}>Entrar</Text>
+          </TouchableHighlight>
+        {/* </View> */}
       </Animatable.View>
     </View>
   )
@@ -121,32 +123,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     color: '#d1d1d1',
   },
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#d1d1d1',
+    width: '60%',
+    borderWidth: 1,
+    overflow: 'hidden',
+  },
   input: {
     height: 50,
     color: '#fff',
     fontSize: 20,
+    borderRadius: 5,
+    borderBottomWidth: 1,
+    borderWidth: 1,
+    borderColor: '#cacaca',
+    paddingLeft: 16,
   },
-  button: {
-    // width: '90%',
-    height: 50,
-    backgroundColor: '#ededed',
-    borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 10,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#d1d1d1',
-  },
-  registerText: {
+  buttonRegisterText: {
     fontSize: 20,
     color: '#ededed',
+    textDecorationLine: 'underline',
   },
   buttonRegister: {
     padding: 8,
     margin: 48,
-    
     borderColor: 'green',
     color: 'red',
     // borderWidth: 1,
@@ -158,19 +161,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  message: {
-    fontSize: 20,
-    color: '#d1d1d1',
-  },
   wrapper: {
     backgroundColor: 'transparent',
-    padding: 10,
-    borderBottomWidth: 1, // adiciona borda inferior
+    padding: 2,
+    borderWidth: 1, // adiciona borda inferior
     fontSize: 20,
     borderColor: '#cacaca',
     width: '50%',
     marginBottom: 26,
     color: '#efefef',
+    borderRadius: 8,
 
   },
   wrapperFocused: {
